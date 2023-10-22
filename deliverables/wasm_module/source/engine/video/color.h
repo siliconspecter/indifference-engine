44b7d40f4cdca743c908043ec872d2f803fcfb69
siliconspecter <siliconspecter@outlook.com>
2023-10-22 13:13:56 +0100

    Specify how color channels are used.

--- deliverables/wasm_module/source/engine/video/color.h
+++ deliverables/wasm_module/source/engine/video/color.h
@@ -10,27 +10,32 @@
 #define COLOR_CHANNELS 4
 
 /**
- * The index of the red channel in a color.
+ * The index of the red channel in a color.  This is an intensity value, where 0
+ * is as dark as possible and 255 is as bright as possible.
  */
 #define COLOR_RED 0
 
 /**
- * The index of the green channel in a color.
+ * The index of the green channel in a color.  This is an intensity value, where
+ * 0 is as dark as possible and 255 is as bright as possible.
  */
 #define COLOR_GREEN 1
 
 /**
- * The index of the blue channel in a color.
+ * The index of the blue channel in a color.  This is an intensity value, where
+ * 0 is as dark as possible and 255 is as bright as possible.
  */
 #define COLOR_BLUE 2
 
 /**
- * The index of the opacity channel in a color.
+ * The index of the opacity channel in a color.  0 is fully transparent and 255
+ * is fully opaque.
  */
 #define COLOR_OPACITY 3
 
 /**
- * A matrix.
+ * A 32-bits-per-pixel red-green-blue-opacity color.  Opacity is NOT
+ * pre-multiplied.
  */
 typedef byte color[COLOR_CHANNELS];
 

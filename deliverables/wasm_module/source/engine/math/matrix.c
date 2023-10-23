e2cc1212e1e8133cb1deb42f152395e5fe04cd05
siliconspecter <siliconspecter@outlook.com>
2023-10-23 01:29:29 +0100

    Add missing implementations.

---
+++ deliverables/wasm_module/source/engine/math/matrix.c
@@ -0,0 +1,7 @@
+#include "matrix.h"
+
+const matrix identity_matrix = {
+    {1.0f, 0.0f, 0.0f, 0.0f},
+    {0.0f, 1.0f, 0.0f, 0.0f},
+    {0.0f, 0.0f, 1.0f, 0.0f},
+    {0.0f, 0.0f, 0.0f, 1.0f}};
---
+++ deliverables/wasm_module/source/engine/math/matrix_pair.c
@@ -0,0 +1,11 @@
+#include "matrix_pair.h"
+
+const matrix_pair identity_matrix_pair = {
+    {{1.0f, 0.0f, 0.0f, 0.0f},
+     {0.0f, 1.0f, 0.0f, 0.0f},
+     {0.0f, 0.0f, 1.0f, 0.0f},
+     {0.0f, 0.0f, 0.0f, 1.0f}},
+    {{1.0f, 0.0f, 0.0f, 0.0f},
+     {0.0f, 1.0f, 0.0f, 0.0f},
+     {0.0f, 0.0f, 1.0f, 0.0f},
+     {0.0f, 0.0f, 0.0f, 1.0f}}};

9304427209d91d3abc4b2691b94703e166df742c
siliconspecter <siliconspecter@outlook.com>
2023-10-23 01:15:53 +0100

    Reduce verbosity.

--- deliverables/wasm_module/source/engine/math/vector.h
+++ deliverables/wasm_module/source/engine/math/vector.h
@@ -5,21 +5,21 @@
 #include "../primitives/f32.h"
 
 /**
- * The number of components in a three-dimensional vector.
+ * The number of components in a 3D vector.
  */
 #define VECTOR_COMPONENTS 3
 
 /**
- * A three-dimensional vector.
+ * A 3D vector.
  */
 typedef f32 vector[VECTOR_COMPONENTS];
 
 /**
- * Calculates the component-wise sum of two three-dimensional vectors.
- * @param augend The three-dimensional vector to add to.
- * @param addend The three-dimensional vector to add by.
- * @param sum The three-dimensional vector to write the result to.  May be
- *               either or both of the operands.
+ * Calculates the component-wise sum of two vectors.
+ * @param augend The vector to add to.
+ * @param addend The vector to add by.
+ * @param sum The vector to write the result to.  May be either or both of the
+ *            operands.
  */
 void add_vectors(
     const vector augend,
@@ -27,11 +27,11 @@ void add_vectors(
     vector sum);
 
 /**
- * Calculates the component-wise difference of two three-dimensional vectors.
- * @param minuend The three-dimensional vector to subtract from.
- * @param subtrahend The three-dimensional vector to subtract by.
- * @param difference The three-dimensional vector to write the result to.  May
- *                   be either or both of the operands.
+ * Calculates the component-wise difference of two vectors.
+ * @param minuend The vector to subtract from.
+ * @param subtrahend The vector to subtract by.
+ * @param difference The vector to write the result to.  May be either or both
+ *                   of the operands.
  */
 void subtract_vectors(
     const vector minuend,
@@ -39,12 +39,10 @@ void subtract_vectors(
     vector difference);
 
 /**
- * Calculates the component-wise product of a three-dimensional vector and a
- * scalar.
- * @param multiplier The three-dimensional vector to multiply.
+ * Calculates the component-wise product of a vector and a scalar.
+ * @param multiplier The vector to multiply.
  * @param multiplicand The scalar to multiply by.
- * @param product The three-dimensional vector to write the result to.  May be
- *                the multiplier.
+ * @param product The vector to write the result to.  May be the multiplier.
  */
 void multiply_vector_by_scalar(
     const vector multiplier,
@@ -52,19 +50,19 @@ void multiply_vector_by_scalar(
     vector product);
 
 /**
- * Calculates the dot product of two three-dimensional vectors.
- * @param multiplier The first three-dimensional vector to combine.
- * @param multiplicand The second three-dimensional vector to combine.
- * @return The dot product of the given three-dimensional vectors.
+ * Calculates the dot product of two vectors.
+ * @param multiplier The first vector to combine.
+ * @param multiplicand The second vector to combine.
+ * @return The dot product of the given vectors.
  */
 f32 dot_product(
     const vector multiplier,
     const vector multiplicand);
 
 /**
- * Calculates the square of the distance between two three-dimensional vectors.
- * @param a The first three-dimensional vector to measure.
- * @param b The second three-dimensional vector to measure.
+ * Calculates the square of the distance between two vectors.
+ * @param a The first vector to measure.
+ * @param b The second vector to measure.
  * @return The square of the distance between the given three-dimensional
  *         vectors.
  */
@@ -73,15 +71,14 @@ f32 distance_squared(
     const vector b);
 
 /**
- * Linearly interpolates between two three-dimensional vectors using a scalar.
- * @param origin The three-dimensional vector to linearly interpolate from.
+ * Linearly interpolates between two vectors using a scalar.
+ * @param origin The vector to linearly interpolate from.
  * @param progress The progress along the linear interpolation, where 0 is the
  *                 origin and 1 is the destination.  Extrapolates beyond that
  *                 range.
- * @param destination The three-dimensional vector to linearly interpolate
- *                    towards.
- * @param interpolated The three-dimensional vector to write the result to.  May
- *                     be any three-dimensional vector operand.
+ * @param destination The vector to linearly interpolate towards.
+ * @param interpolated The vector to write the result to.  May be any vector
+ *                     operand.
  */
 void linearly_interpolate_vectors_by_scalar(
     const vector origin,

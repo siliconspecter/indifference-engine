2b41e3d8373bbd8b02e91a447d7c9bb235e01af4
siliconspecter <siliconspecter@outlook.com>
2023-10-27 19:49:09 +0100

    Don't export the width of the video buffer; this is implicit.

--- deliverables/wasm_module/source/engine/exports/buffers/index.c
+++ deliverables/wasm_module/source/engine/exports/buffers/index.c
@@ -14,32 +14,30 @@
 const s32 ticks_per_second = TICKS_PER_SECOND;
 const s32 samples_per_tick = SAMPLES_PER_TICK;
 const s32 maximum_video_rows = MAXIMUM_VIDEO_ROWS;
-const s32 maximum_video_columns = MAXIMUM_VIDEO_COLUMNS;
 const s32 safe_area_rows = SAFE_AREA_ROWS;
 const s32 safe_area_columns = SAFE_AREA_COLUMNS;
 
-#define BUFFER_LIST(item)                                                                                         \
-  item(1073741824, ticks_per_second)                                                                              \
-      item(1073741825, samples_per_tick)                                                                          \
-          item(1073741826, audio_buffer)                                                                          \
-              item(1073741827, maximum_video_rows)                                                                \
-                  item(1073741828, maximum_video_columns)                                                         \
-                      item(1073741829, safe_area_rows)                                                            \
-                          item(1073741830, safe_area_columns)                                                     \
-                              item(1073741831, video_buffer)                                                      \
-                                  item(1073741832, pointer_state)                                                 \
-                                      item(1073741833, pointer_row)                                               \
-                                          item(1073741834, pointer_column)                                        \
-                                              item(1073741835, persist)                                           \
-                                                  item(1073741836, tick_progress)                                 \
-                                                      item(1073741837, video_rows)                                \
-                                                          item(1073741838, video_columns)                         \
-                                                              item(1073741839, listener_location)                 \
-                                                                  item(1073741840, listener_normal)               \
-                                                                      item(1073741841, controller_states)         \
-                                                                          item(1073741842, controller_y_axes)     \
-                                                                              item(1073741843, controller_x_axes) \
-                                                                                  item(1073741844, error)
+#define BUFFER_LIST(item)                                                                                     \
+  item(1073741824, ticks_per_second)                                                                          \
+      item(1073741825, samples_per_tick)                                                                      \
+          item(1073741826, audio_buffer)                                                                      \
+              item(1073741827, maximum_video_rows)                                                            \
+                  item(1073741828, safe_area_rows)                                                            \
+                      item(1073741839, safe_area_columns)                                                     \
+                          item(1073741830, video_buffer)                                                      \
+                              item(1073741831, pointer_state)                                                 \
+                                  item(1073741832, pointer_row)                                               \
+                                      item(1073741833, pointer_column)                                        \
+                                          item(1073741834, persist)                                           \
+                                              item(1073741835, tick_progress)                                 \
+                                                  item(1073741836, video_rows)                                \
+                                                      item(1073741837, video_columns)                         \
+                                                          item(1073741838, listener_location)                 \
+                                                              item(1073741839, listener_normal)               \
+                                                                  item(1073741840, controller_states)         \
+                                                                      item(1073741841, controller_y_axes)     \
+                                                                          item(1073741842, controller_x_axes) \
+                                                                              item(1073741843, error)
 
 // TODO: macro for NULL
 #define BUFFER_NULL(identifier, data) (void *)0,

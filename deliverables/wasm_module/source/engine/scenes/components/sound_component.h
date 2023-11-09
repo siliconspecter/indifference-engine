ea7a6475fe9f2746139beac845fdabeb2a572d48
siliconspecter <siliconspecter@outlook.com>
2023-11-09 23:26:47 +0000

    Fix names.

--- deliverables/wasm_module/source/engine/scenes/components/sound_component.h
+++ deliverables/wasm_module/source/engine/scenes/components/sound_component.h
@@ -55,8 +55,8 @@ typedef void(sound_component_ended)(index meta);
 component_handle sound_component(
     const index entity,
     const index meta,
-    const sound_component_replaced *const on_tick,
-    const sound_component_ended *const on_expiry,
+    const sound_component_replaced *const on_replacement,
+    const sound_component_ended *const on_end,
     const sound *const sound);
 
 /**
@@ -85,8 +85,8 @@ component_handle sound_component(
 component_handle sound_sub_component(
     const component_handle component,
     const index meta,
-    const sound_component_replaced *const on_tick,
-    const sound_component_ended *const on_expiry,
+    const sound_component_replaced *const on_replacement,
+    const sound_component_ended *const on_end,
     const sound *const sound);
 
 /**

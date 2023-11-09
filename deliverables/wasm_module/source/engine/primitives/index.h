31b9f83cbcc0527325314957594feb84eb7830b5
siliconspecter <siliconspecter@outlook.com>
2023-11-09 23:28:04 +0000

    Migrate index maintenance helpers.

--- deliverables/wasm_module/source/engine/primitives/index.h
+++ deliverables/wasm_module/source/engine/primitives/index.h
@@ -26,4 +26,96 @@ typedef s32 index;
  */
 #define INDEX_NONE -1
 
+/**
+ * Finds an vacant slot within a field of indices.  Attempts to maintain as
+ * small and contiguous a range of used indices as possible.
+ * @param vacancies An array which can be used to check for occupancy.
+ * @param vacant A constant which indicates a vacancy in the array of potential
+ *               vacancies.
+ * @param count The number of slots.
+ * @param first_occupied The name of the previously defined variable containing
+ *                       the index of the first occupied slot.  This must be
+ *                       INDEX_NONE if no slots are occupied.
+ * @param last_occupied The name of the previously defined variable containing
+ *                      the index of the last occupied slot.
+ * @param error The error which is raised if there are no vacancies.
+ * @param output The name of the variable to declare and populate with the index
+ *               of the vacant slot.
+ */
+#define FIND_EMPTY_INDEX(vacancies, vacant, count, first_occupied, last_occupied, error, output) \
+  index output;                                                                                  \
+                                                                                                 \
+  if (first_occupied == INDEX_NONE)                                                              \
+  {                                                                                              \
+    output = 0;                                                                                  \
+    first_occupied = 0;                                                                          \
+    last_occupied = 0;                                                                           \
+  }                                                                                              \
+  else                                                                                           \
+  {                                                                                              \
+    for (output = first_occupied; output < last_occupied; output++)                              \
+    {                                                                                            \
+      if (vacancies[output] == vacant)                                                           \
+      {                                                                                          \
+        break;                                                                                   \
+      }                                                                                          \
+    }                                                                                            \
+                                                                                                 \
+    if (output == last_occupied)                                                                 \
+    {                                                                                            \
+      output++;                                                                                  \
+                                                                                                 \
+      if (output == count)                                                                       \
+      {                                                                                          \
+        if (first_occupied == 0)                                                                 \
+        {                                                                                        \
+          throw(error);                                                                          \
+        }                                                                                        \
+        else                                                                                     \
+        {                                                                                        \
+          output = first_occupied - 1;                                                           \
+          first_occupied = output;                                                               \
+        }                                                                                        \
+      }                                                                                          \
+      else                                                                                       \
+      {                                                                                          \
+        last_occupied = output;                                                                  \
+      }                                                                                          \
+    }                                                                                            \
+  }
+
+/**
+ * Maintains look-up state when a previously occupied slot within a field of
+ * indices is vacated.
+ * @param vacated The index of the newly vacated slot.
+ * @param vacancies An array which can be used to check for occupancy.
+ * @param vacant A constant which indicates a vacancy in the array of potential
+ *               vacancies.
+ * @param first_occupied The name of the previously defined variable containing
+ *                       the index of the first occupied slot.  This must be
+ *                       INDEX_NONE if no slots are occupied.
+ * @param last_occupied The name of the previously defined variable containing
+ *                      the index of the last occupied slot.
+ */
+#define INDEX_VACATED(vacated, vacancies, vacant, first_occupied, last_occupied) \
+  if (vacated == first_occupied)                                                 \
+  {                                                                              \
+    if (vacated == last_occupied)                                                \
+    {                                                                            \
+      first_occupied = INDEX_NONE;                                               \
+    }                                                                            \
+    else                                                                         \
+      do                                                                         \
+      {                                                                          \
+        first_occupied++;                                                        \
+      } while (vacancies[first_occupied] == vacant);                             \
+  }                                                                              \
+  else if (vacated == last_occupied)                                             \
+  {                                                                              \
+    do                                                                           \
+    {                                                                            \
+      last_occupied--;                                                           \
+    } while (vacancies[last_occupied] == vacant);                                \
+  }
+
 #endif

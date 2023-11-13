84d254b1d38b88ed9b7b10737c2da8b491bb2145
siliconspecter <siliconspecter@outlook.com>
2023-11-13 01:23:16 +0000

    Fix build error.

--- deliverables/wasm_module/source/engine/exports/event_handlers/video.c
+++ deliverables/wasm_module/source/engine/exports/event_handlers/video.c
@@ -3,15 +3,20 @@
 #include "../buffers/video.h"
 #include "../../scenes/entity.h"
 #include "../../scenes/components/camera_component.h"
+#include "../../primitives/f32.h"
 
-#include "../../video/color.h"
-#include "../../video/line.h"
-#include "../buffers/error.h"
-#include "../../math/matrix.h"
+static void render()
+{
+  copy_f32(1.0f, camera_component_depths, camera_component_rows * camera_component_columns);
+}
 
 EXPORT void video()
 {
   initialize_event_handler();
   inverse_tick_progress = 1.0f - tick_progress;
+
+  copy_f32(0.0f, camera_component_opacities, video_rows * video_columns);
+
   prepare_entities_for_video();
+  render_camera_components(render);
 }

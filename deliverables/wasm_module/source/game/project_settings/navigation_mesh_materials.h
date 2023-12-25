5c38dab4a6657e19ffdcee8c6c1f6a5c771ca082
siliconspecter <siliconspecter@outlook.com>
2023-12-25 11:52:45 +0000

    Add default navigation mesh materials.

---
+++ deliverables/wasm_module/source/game/project_settings/navigation_mesh_materials.h
@@ -0,0 +1,19 @@
+/** @file */
+
+#ifndef NAVIGATION_MESH_MATERIALS_H
+
+#define NAVIGATION_MESH_MATERIALS_H
+
+#include "../../engine/primitives/s32.h"
+
+/**
+ * A material applied to a navigation mesh.
+ */
+typedef s32 navigation_mesh_material;
+
+/**
+ * The default navigation mesh material.
+ */
+#define NAVIGATION_MESH_MATERIAL_DEFAULT 0
+
+#endif
